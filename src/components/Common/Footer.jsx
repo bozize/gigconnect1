0ed95bb18e7fa930a0218c38b3bae74cdca8d539
boziz <bozize@gmail.com>
2024-09-23 19:17:29 +0300

    connected to the backend

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto w-full md:w-3/4 text-center">
+      <div className="container mx-auto px-4 md:px-0 w-full text-center">
         <p className="text-sm md:text-base">&copy; 2024 GigConnect. All rights reserved.</p>
-        <ul className="flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-6 mt-4">
+        <ul className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-6 mt-4">
           <li>
             <a href="#" className="hover:text-[#5BC0BE] transition duration-300">
               Privacy Policy
@@ -20,7 +20,7 @@ const Footer = () => {
             </a>
           </li>
         </ul>
-        <div className="flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-4 mt-4">
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-4 mt-4">
           <a href="#" className="hover:text-[#5BC0BE] transition duration-300">
             <i className="fab fa-facebook-f"></i>
           </a>
@@ -40,6 +40,7 @@ const Footer = () => {
 };
 
 export default Footer;
+
 
 
 
